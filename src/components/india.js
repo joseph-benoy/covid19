8bbc60280695692bcc8713c1586374fefbe2638c
Joseph Benoy <josephbenoy03@gmail.com>
2021-06-04 12:39:52 +0530

    added india axios

--- src/components/india.js
+++ src/components/india.js
@@ -1,8 +1,26 @@
 import React,{Component} from 'react';
+import axios from 'axios';
 
 export default class India extends Component{
     constructor(props){
         super(props);
+        this.state = {
+            states:{}
+        };
+    }
+    componentDidMount(){
+        axios.get("https://api.covid19india.org/state_district_wise.json")
+        .then((response)=>{
+            let statesList = [];
+            for(let i in response.data){
+                statesList.push({stateName:i,data:response.data[i]});
+            }
+            this.setState({states:statesList});
+            console.log(statesList);
+        })
+        .catch((error)=>{
+            console.log(error);
+        });
     }
     render(){
         return(<h1>India</h1>);
